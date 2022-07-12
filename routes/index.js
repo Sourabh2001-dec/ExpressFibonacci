ce198e12210ed3ba8d44292f7b1d51fb9903f03e
Sourabh Kumbhar <55979623+Sourabh2001-dec@users.noreply.github.com>
2022-07-12 18:39:59 +0530

    ecommerce web app

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,17 @@
 var express = require("express");
 var router = express.Router();
 
-// function to calculate fibonacci number
-function fibonacci(n) {
-    if (typeof n !== "number") return "Please enter a number";
-    if (n <= 1) return n;
-    return fibonacci(n - 1) + fibonacci(n - 2);
-}
+const products = require("../products.json");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-    res.render("index", { time: new Date().toISOString() });
+    res.render("index", { products: products });
 });
 
-router.post("/", function (req, res, next) {
-    const start = Date.now();
-    const ans = fibonacci(Number(req.body.num));
-    const end = Date.now();
-    const duration = end - start;
-    res.render("index", {
-        time: new Date().toISOString(),
-        fib: ans,
-        duration: duration,
-    });
+router.get("/products/:id", function (req, res, next) {
+    const id = +req.params.id;
+    const product = products.find((product) => product.id === id);
+    res.render("product", { product: product });
 });
 
 module.exports = router;
