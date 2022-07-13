facb16421fdee1af27aac43a3fceb8d490ffabae
Sourabh Kumbhar <55979623+Sourabh2001-dec@users.noreply.github.com>
2022-07-13 11:05:13 +0530

    Added cpu stress code

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,25 @@
 var express = require("express");
 var router = express.Router();
+const { exec } = require("child_process");
 
 const products = require("../products.json");
+
+function cpuStress() {
+    // run stress shell command using node js
+    // Gives cpu load upto 60%
+    exec("stress -c 3 -t 3s", (err, stdout, stderr) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        // console.log(stdout);
+    });
+}
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
     res.render("index", { products: products });
+    cpuStress();
 });
 
 router.get("/products/:id", function (req, res, next) {
